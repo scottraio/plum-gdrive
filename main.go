8238e9b7183c4338af310c0146136c08ff83fb6a
Scott Raio <scottraio@gmail.com>
2023-06-20 09:04:39 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"context"
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v3"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	Run()
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func Run() {
+	ctx := context.Background()
+	b, err := os.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(config)
+
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+	if err != nil {
+		log.Fatalf("Unable to retrieve Drive client: %v", err)
+	}
+
+	folderID := "1E8_BwQAL2r2N8wkZBZ_JRGdvqqwvm4hc"
+	r, err := srv.Files.List().SupportsTeamDrives(true).IncludeItemsFromAllDrives(true).Q("'" + folderID + "' in parents").Fields("files(id, name, mimeType)").Do()
+
+	if err != nil {
+		log.Fatalf("Unable to retrieve files: %v", err)
+	}
+	fmt.Println("Files:")
+	if len(r.Files) == 0 {
+		fmt.Println("No files found.")
+	} else {
+		var parts []string
+		for _, i := range r.Files {
+			//
+			exportURL := srv.Files.Export(i.Id, "text/csv")
+
+			resp, err := exportURL.Download()
+			if err != nil {
+				log.Fatalf("Unable to retrieve data from file: %v", err)
+			}
+
+			defer resp.Body.Close()
+
+			// Read the exported CSV data
+			data, _ := ioutil.ReadAll(resp.Body)
+			parts = append(parts, findParts(string(data))...)
+
+			fmt.Printf("%s (%s)\n", i.Name, i.Id)
+		}
+
+		for _, part := range findUniqueParts(parts) {
+			http.Get("http://localhost:8080/mark_as_sellable/" + part)
+		}
+	}
+}
+
+func findParts(csvString string) []string {
+	reader := csv.NewReader(strings.NewReader(csvString))
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil
+	}
+
+	var partNumbers []string
+
+	for _, row := range records {
+		for columnIndex, column := range row {
+			if columnIndex > 1 && isValidPartNumber(column) {
+				partNumbers = append(partNumbers, column)
+			}
+		}
+	}
+
+	return partNumbers
+}
+
+func findUniqueParts(partNumbers []string) []string {
+	uniqueParts := make([]string, 0, len(partNumbers))
+	encountered := make(map[string]bool)
+
+	for _, part := range partNumbers {
+		if !encountered[part] {
+			uniqueParts = append(uniqueParts, part)
+			encountered[part] = true
+		}
+	}
+
+	return uniqueParts
+}
+
+func isValidPartNumber(partNumber string) bool {
+	// Check if the part number consists of one word
+	if strings.Contains(partNumber, " ") {
+		return false
+	}
+
+	// Check if the length is between 5 and 10 characters
+	length := len(partNumber)
+	return length >= 4 && length <= 15
+}
